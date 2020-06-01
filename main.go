ff1652afed8f801d1acd78633385cbd207f6fb5b
Devin Black <Devinjamesblack@gmail.com>
2020-06-01 05:12:30 -0600

    First branch off

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to my first Golang site!</h1>")
+		fmt.Fprint(w, "<h1>Welcome to my first Golang site! Making changes to our work</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "To get in touch, please send an email "+
 			"to <a href=\"mailto:support@lenslocked.com\">"+"support@lenslocked.com</a>.")
